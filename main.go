17b360baa7e0c25504d814459992db84f83bcab2
Khairul Akmal <khairulakmal.dev@gmail.com>
2023-12-07 23:05:15 +0800

    chore: running the exercise

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
-import (
-	"github.com/mofodox/gobootcamp/tutorials"
-)
+import "github.com/mofodox/gobootcamp/exercises"
 
 func main() {
 	/*
 		Uncomment the ones that you wanted to learn
 	*/
-	tutorials.TutorialOne()
+	// tutorials.TutorialOne()
 	// tutorials.TutorialTwo()
 	// tutorials.TutorialThree()
 	// tutorials.TutorialFour()
+	// tutorials.TutorialFive()
+	// tutorials.TutorialSix()
+	// tutorials.TutorialSeven()
+	// tutorials.TutorialEight()
+	// tutorials.TutorialNine()
 
 	/*
 		Exercises based on the tutorials
@@ -20,4 +23,5 @@ func main() {
 	*/
 	// exercises.ExerciseOneToThreeTutorial()
 	// exercises.Exercise4Tutorial()
+	exercises.Exercise5Tutorial()
 }
